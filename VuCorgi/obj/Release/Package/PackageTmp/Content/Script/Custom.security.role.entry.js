b20e452f3dec74ca5a1e713783e6579c4d865a5f
Sean Nguyen <thinhspgood@gmail.com>
2021-01-28 15:19:57 +0700

    first

---
+++ VuCorgi/obj/Release/Package/PackageTmp/Content/Script/Custom.security.role.entry.js
@@ -0,0 +1,82 @@
+﻿$('#role-table tbody').on('change', 'input.is-menu-active', function () {
+    var value = $(this).closest('td').find('input.menu-active-value');
+    if ($(this).is(':checked')) {
+        value.val(true);
+        //$(this).addAttr('checked');
+        //$(this).attr('checked', 'checked');
+
+        // Also Check all parent Menu-Item
+        var parentId = parseInt($(this).attr('data-parent-id'));
+        checkAllParentMenu(parentId);
+    } else {
+        value.val(false);
+        //$(this).removeAttr('checked');
+        // Uncheck All Childs MenuItem
+        var id = parseInt($(this).attr('data-id'));
+        uncheckAllChildsMenu(id)
+    }
+});
+
+//$('input.is-menu-active').change(function () {
+//    var value = $(this).closest('td').find('input.menu-active-value');
+//    if ($(this).is(':checked')) {
+//        value.val(true);
+//        //$(this).addAttr('checked');
+//        //$(this).attr('checked', 'checked');
+
+//        // Also Check all parent Menu-Item
+//        var parentId = parseInt($(this).attr('data-parent-id'));
+//        checkAllParentMenu(parentId);
+//    } else {
+//        value.val(false);
+//        //$(this).removeAttr('checked');
+//        // Uncheck All Childs MenuItem
+//        var id = parseInt($(this).attr('data-id'));
+//        uncheckAllChildsMenu(id)
+//    }
+//});
+
+
+function checkAllParentMenu(parentId) {
+    var parentCheckbox = $("#role-table").find('input[data-id=' + parentId + ']');
+    if(parentCheckbox !== null){
+        if(parentCheckbox.hasOwnProperty('length')){
+            if(parentCheckbox.length >0){
+                parentCheckbox.each(function () {
+                    var parentValue = $(this).closest('td').find('input.menu-active-value');
+                    parentValue.val(true);
+                    if (!$(this).is(':checked')) {
+                        $(this).checked = true;
+                        $(this).attr('checked', 'checked');
+                        $(this).prop("checked", true);
+                    }
+                    var upperLevelId = parseInt($(this).attr('data-parent-id'));
+                    if (upperLevelId !== -1) {
+                        checkAllParentMenu(upperLevelId);
+                    }
+                });
+            }
+        }
+    }
+}
+
+function uncheckAllChildsMenu(id) {
+    var childList = $("#role-table").find('input[data-parent-id=' + id + ']');
+    if (childList !== null) {
+        if (childList.hasOwnProperty("length")) {
+            if (childList.length > 0) {
+                childList.each(function () {
+                    var childCheckBox = $(this);
+                    var childValue = childCheckBox.closest('td').find('input.menu-active-value');
+                    childValue.val(false);
+                    childCheckBox.removeAttr('checked');
+                    childCheckBox.checked = false;
+                    $(this).prop("checked", false);
+                    var childMenuId = parseInt(childCheckBox.attr('data-id'));
+                    uncheckAllChildsMenu(childMenuId);
+                });
+            }
+        }
+    }
+
+}
---
+++ VuCorgi/obj/Release/Package/PackageTmp/Scripts/JavaHelper/FileManagement.js
@@ -0,0 +1,26 @@
+﻿/// Use this file when the page has a button with none display.///
+///please insert <input id="inputFile" type="file" multiple="multiple" accept="image/*" style="display:none" />
+function Savefile(listfile, editor, url) {
+    openHoldOn();
+    for (var i = 0; i < listfile.length; i++) {
+        var formData = new FormData();
+        formData.append('myFile', listfile[i]);
+        $.ajax({
+            url: url,
+            type: 'POST',
+            data: formData,
+            dataType: 'json',
+            contentType: false,
+            processData: false,
+            success: function (data) {
+                if (data[0] === 'Fail') {
+                    alert(data[1]);
+                }
+                else {
+                    editor.insertHtml('<img src="' + data + '"/>');
+                }
+            }
+        });
+    }
+    closeHoldOn();
+}
